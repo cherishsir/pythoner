47c83f4aa714f316883ffbb017fc52abbc819953
cherishsir <timehuang@hust.edu.cn>
2014-06-27 22:43:19 +0800

    修改的更具有模块性

--- wangyicourse.py
+++ wangyicourse.py
@@ -4,44 +4,57 @@ import  os
 from  urllib2  import *
 from BeautifulSoup import BeautifulSoup
 import sys
-def printall(start,titlelist):
+def printall(end,titlelist):
 	print "你将下载的视频如下:"
-	for i in range(start,len(titlelist)):
+	for i in range(0,end):
 		print titlelist[i]
 	raw_input("按下任意键,开始下载")
 	
 
-def getstart(titlelist):
-	first=titlelist[1]
-	for i in range(2,len(titlelist)):
-		if first==titlelist[i]:
-			return i
-	return 0
-def gettitle(url,varlist):
+def getstart(alist):
+	i=0
+	first=alist[i]
+	while first=="None":
+		#print "haha"
+		i+=1
+		first=alist[i]
+	newlist=[]
+	b=0
+	for i in range(1,len(alist)):
+		if first==alist[i]:
+			break
+	for a in range(i,len(alist)):
+		#print "*inside getstart*",alist[a]
+		newlist.append(alist[a])
+
+	return newlist
+def gettitle(url,varlist,keywords):
 	html=urlopen(url).read()
 	soup=BeautifulSoup(html,fromEncoding="gb18030")
 	for i in  soup.findAll("a"):
-	 if "http://v.163.com/movie" in str(i):
+	 if keywords in str(i):
+		#print i
 		titleurl=str(i.string).strip()
 		titleurl=titleurl.replace(" ","")
 		varlist.append(titleurl.replace(" - ","").strip())	
 
-def getmp4(url,varlist):
+def geturl(url,varlist,keywords):
 	html=urlopen(url).read()
 	soup=BeautifulSoup(html)
 	for i in  soup.findAll("a"):
-	 if ".mp4" in str(i):
+	 if keywords in str(i):
+		#print str(i);
 		mp4url=str(i["href"]).strip()
 		varlist.append(mp4url)	
 
 
-def download(start,varlist,titlelist):
+def download(start,urllist,titlelist):
 	number=1
-	for i in range(start,len(varlist)):
+	for i in range(start,len(urllist)):
 		where="第"+str(number)+"课"
 		filename=where+str(titlelist[i])+".mp4"
 		if not os.path.isfile(filename): # do not have this file in local
-			all="wget "+str(varlist[i])+" -O "+filename
+			all="wget "+str(urllist[i])+" -O "+filename
 			#print all
 			os.system(all)
 			number+=1
@@ -54,37 +67,16 @@ def download(start,varlist,titlelist):
 if __name__=="__main__":
 	url=raw_input("请输入你要下载的网页>:")
 	#url="http://v.163.com/special/opencourse/classicalmechanics.html"
-	mp4list=[" ",] 				#save http://xx.mp4
+	mp4list=[] 				#save http://xx.mp4
 	titlelist=[]				#save title
-	if  len(sys.argv)==2:
-		os.makedirs(sys.argv[1]) 	#mdir a dir
-		savedir=sys.argv[1]
-	getmp4(url,mp4list)	
-	gettitle(url,titlelist)
-	start=getstart(titlelist)
-	printall(start,titlelist)
+	geturl(url,mp4list,".mp4")
+	gettitle(url,titlelist, "http://v.163.com/movie")
 
-	number=1
-	for i in range(start,len(mp4list)):
-		where="第"+str(number)+"课"
-		filename=where+str(titlelist[i])+".mp4"
-		if not os.path.isfile(filename): # do not have this file in local
-			all="wget "+str(mp4list[i])+" -O "+filename
-			#print all
-			os.system(all)
-			number+=1
-		
-		else:
-			print filename,"已经存在,skipping"
-			number+=1
+#去掉重复的url and title,返回值为没有重复的url and title
+	mp4list=getstart(mp4list)
+	titlelist=getstart(titlelist)
 
-		if  len(sys.argv)==2:
-			os.system("mv "+filename+" "+sys.argv[1])
-		
-
-
-
-
-
-
+#下载对应的内容,通过mp4list找下载对应的视频，mp4list的len决定了视频的数量
+	printall(len(mp4list),titlelist)
+	download(0,mp4list,titlelist)
 
